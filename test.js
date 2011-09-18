73e0e4b021bb218eda55c83379e140306d74dab6
Aikar <aikar@aikar.co>
2011-09-18 10:52:53 -0400

    updates Moving config to its own class. Singleton improvements (enforce) Config test

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
 var nodeib = require('nodeib');
 console.log(nodeib.getModulePaths('foo', 'bar'));
+console.log(Config.get('foo', 1))
+console.log(Config.get('bar', 2))
+console.log(Config.get('baz.foo', 3))
+console.log(Config.get('baz', 4))
